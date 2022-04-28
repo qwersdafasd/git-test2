5ae0608b0637c07c39aff09535f0a30bd77e6758
ws <2804516739@qq.com>
2022-04-28 17:44:20 +0800

    third commit

--- src/main/java/com/ws/git/git_demon01.java
+++ src/main/java/com/ws/git/git_demon01.java
@@ -4,5 +4,6 @@ public class git_demon01 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
